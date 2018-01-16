63b9dd128918356b0f4b5c195d2fafa7678facb1
maymashd <maymashd@mail.ru>
2018-01-17 01:11:20 +0600

    week2 minimim and maximum from the file

---
+++ Week2/Minimum and maximum/Minimum and maximum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace maximum_and_mimimum
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            
+            string line2 =  File.ReadAllText(@"C:\Users\User\desktop\PP2labs\Week2\Minimum and maximum\test.txt");
+           
+
+
+            string[] line = line2.Split(' ');
+
+            int maxx = -10000000;
+            int minn = 10000000;
+            foreach (string s in line)
+            {
+                maxx = Math.Max(maxx, int.Parse(s));
+                minn = Math.Min(minn, int.Parse(s));
+
+            }
+            Console.WriteLine("Minimum number is ");
+            Console.WriteLine(minn);
+            Console.WriteLine("Maximum number is");
+            Console.WriteLine(maxx);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Week2/complex number/complex number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complex_number
+{
+    class complex
+    {
+        i
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
