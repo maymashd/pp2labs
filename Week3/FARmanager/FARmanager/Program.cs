3233511149715890bbe872075f82fd45bf3be4fc
maymashd <maymashd@mail.ru>
2018-01-29 23:30:40 +0600

    I change my code, i wrote with stack

--- Week3/FARmanager/FARmanager/Program.cs
+++ Week3/FARmanager/FARmanager/Program.cs
@@ -4,115 +4,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-namespace FileManager
-{
-    class Program
-    {
+namespace FileManager{
+    class Program{
         static int s = 0,l=0;
-        static DirectoryInfo a = new DirectoryInfo(@"C:\Users\User\Desktop\PP2labs");
+        static DirectoryInfo a = new DirectoryInfo(@"C:\Users\User\Desktop\PP2labs"), q = a;
         static FileSystemInfo[] b = a.GetFileSystemInfos();
-        static DirectoryInfo q = a;
-        static void shower(FileSystemInfo[] t, ConsoleKeyInfo p, int s2)
-        {
-
+        static Stack<int> stack = new Stack<int> () ;
+        static void shower(FileSystemInfo[] t, ConsoleKeyInfo p, int s2){
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Clear();
-            if (p.Key == ConsoleKey.Enter)
-            {
-                if (t[s2].GetType() == typeof(DirectoryInfo))
-                {
+            if (p.Key == ConsoleKey.Enter){
+                stack.Push(s2);
+                if (t[s2].GetType() == typeof(DirectoryInfo)){
                     a = new DirectoryInfo(t[s2].FullName);
                     b = a.GetFileSystemInfos();
                     s = 0;
                     s2 = s;
                     t = b;
                 }
-                else
-                {
-                    string text = File.ReadAllText(t[s2].FullName);
-                   
-                    Console.WriteLine(text);
+                else{
+                    StreamReader sr = new StreamReader(t[s2].FullName);
+                    string st = sr.ReadLine();
+                    Console.WriteLine(st);
                     Console.ReadKey();
                     Console.Clear();
-
-                }
-            }
-            if (p.Key == ConsoleKey.Escape)
-            {
-
-                if (a.FullName == q.FullName)
-                {
+                }}
+            if (p.Key == ConsoleKey.Escape){
+                if (a.FullName == q.FullName){
                     l = 1;
                     return;
                 }    
                 a = a.Parent;
                 b = a.GetFileSystemInfos();
-                s = 0;
+                s2 = stack.Pop();
                 t = b;
             }
             int size = t.Length;
-            if (p.Key == ConsoleKey.DownArrow)
-            {
+            if (p.Key == ConsoleKey.DownArrow){
                 s2++;
                 if (s2 == size)
                     s2 = 0;
-
             }
-            if (p.Key == ConsoleKey.UpArrow)
-            {
+            if (p.Key == ConsoleKey.UpArrow){
                 s2--;
-
                 if (s2 == -1)
                     s2 = size - 1;
             }
-
             int m = 0;
-            foreach (FileSystemInfo g in t)
-            {
-                if (m == s2)
-                {
+            foreach (FileSystemInfo g in t){
+                if (m == s2){
                     Console.BackgroundColor = ConsoleColor.DarkGray;
                     Console.WriteLine(g.Name);
-
                 }
-                else
-
-                {
+                else{
                     Console.BackgroundColor = ConsoleColor.Black;
-                    if (g.GetType() == typeof(FileInfo))
-                    {
+                    if (g.GetType() == typeof(FileInfo)){
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine(g.Name);
                     }
-                    if (g.GetType() == typeof(DirectoryInfo))
-                    {
+                    if (g.GetType() == typeof(DirectoryInfo)){
                         Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine(g.Name);
-                    }
-
-
-                }
+                    } }
                 m++;
             }
             s = s2;
-
         }
-
         static void Main(string[] args)
         {
-
-
             Console.WriteLine("please type a letter 's' to start =)");
-            while (true)
-            {
-
+            while (true){
                 ConsoleKeyInfo pk = Console.ReadKey();
-
                 shower(b, pk, s);
                 if (l == 1)
                     break;
             }
-
         }
     }
 }
